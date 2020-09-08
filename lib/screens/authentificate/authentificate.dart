07cb0e2f8b8ae99de7e8c4f9f672f4cc9adbb08c
diakite Ibrahima sory <diakitemouslim2021@gmail.com>
2020-09-08 06:54:09 +0000

    name change

---
+++ lib/screens/authentificate/authentificate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authenficate extends StatefulWidget {
+  @override
+  _AuthenficateState createState() => _AuthenficateState();
+}
+
+class _AuthenficateState extends State<Authenficate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    child: Text('Authentificate'),
+    );
+  }
+}
